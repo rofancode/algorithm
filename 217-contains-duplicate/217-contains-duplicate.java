c1fb6fdb995e0aa3fe2a168622ea2c422a442203
rofancode <75173954+rofancode@users.noreply.github.com>
2022-08-29 10:32:08 +0900

    Time: 22 ms (30.31%), Space: 75 MB (5.17%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -1,12 +1,13 @@
-import java.util.Arrays;
-
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> set = new HashSet<>(nums.length);
         for (int x: nums) {
-            if (set.contains(x)) return true;
-            set.add(x);
+            if (set.contains(x)) return true; //search()
+            set.add(x); //insert()
         }
+
         return false;
     }
 }
+//time complexity : O(n)
+//space : O(n)
