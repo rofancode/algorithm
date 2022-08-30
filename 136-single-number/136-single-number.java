5f89b8d680963aa9b9ec8ec52387d7cc45bd820f
rofancode <75173954+rofancode@users.noreply.github.com>
2022-08-31 01:34:37 +0900

    Time: 1 ms (100.00%), Space: 50.8 MB (39.74%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,14 +1,12 @@
 class Solution {
     public int singleNumber(int[] nums) {
         
-        Arrays.sort(nums);
-        
-        for (int i = 0; i < nums.length -2; i += 2) {
-            if(nums[i] != nums[i + 1])
-                return nums[i];
+        int a = 0;
+        for (int i :nums) {
+            a ^= i;
         }
         
-        return nums[nums.length - 1];
+        return a;
     }
-
+//TC logN + N
 }
