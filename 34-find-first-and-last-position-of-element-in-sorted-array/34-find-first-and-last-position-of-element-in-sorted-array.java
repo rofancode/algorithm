61af97857e90973f3bde15e3b358f41ce89595d9
rofancode <75173954+rofancode@users.noreply.github.com>
2022-09-01 16:52:03 +0900

    Time: 0 ms (100.00%), Space: 47.5 MB (31.38%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,51 +1,48 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
+        // binary search로 target 찾기
+        // start 찾고, end찾기
         
+        //System.out.println(mid);
+        // target 안될때 까지  start--와 
+        
+        
+        
+        return new int[]{findBinary(nums, target, true),findBinary(nums, target, false)};
+    }
+    // binary searhc logN + N
+    
+    public int findBinary(int[] nums, int target, boolean isFirst) {
+        int n = nums.length;
         int start = 0;
-        int end = nums.length - 1;
+        int end = n - 1;
         
-        int mid = 0;
-        // binary search로 target 찾고
-        while (start <= end ) {
-            mid = end - (end - start) / 2;
-            //mid = (start + end) / 2;
-            System.out.println(mid);
+        while (start <= end) {
+            int mid = end - (end - start) / 2;
             int num = nums[mid];
-            
+            //first 는 nums[mid - 1] != target
             if (num == target) {
-                break;
+                if (isFirst) {
+                    if(mid == 0 || nums[mid - 1] != target) {
+                        return mid;
+                    } else {
+                        end = mid - 1;
+                    }
+                } else {
+                    if(mid == n - 1 || nums[mid + 1] != target) {
+                        return mid;
+                    } else {
+                        start = mid + 1;
+                    }
+                }
             } else if (num > target) {
                 end = mid - 1;
             } else {
                 start = mid + 1;
             }
-            
         }
         
-        if(start > end)
-            return new int[]{-1, -1};
-        
-        //System.out.println(mid);
-        // target 안될때 까지  start--와 
-        
-        for (int i = mid; i >= 0; i--) {
-            if(nums[i] != target) {
-                break;
-            }
-            start = i;
-        }
-        
-        //end++를 
-        for (int i = mid; i < nums.length; i++) {
-            //System.out.println(i+"|"+nums[i]);
-            if(nums[i] != target) {
-                break;
-            }
-            end = i;
-        }
-        
-        return new int[]{start, end};
+        return -1;
     }
-    
     
 }
