5ed0d9f10b6e4e41b6aeb07898616081abfa0c34
rofancode <75173954+rofancode@users.noreply.github.com>
2022-09-10 13:17:45 +0900

    Time: 28 ms (6.71%), Space: 55.3 MB (43.58%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        //sort start, end 순으로 
+        Arrays.sort(intervals, (a, b) -> a[0] == b[0]? a[1] - b[1] : a[0] - b[0]);
+        
+        //input 정해지지 않아서, List에 저장 -> 나중에 전환
+        List<int[]> merged = new ArrayList();
+        
+        //check overlab
+        //처음 start / end 시간 지정 
+        //-> preEnd >= start ; overlap  기존 int[] 데이터 수정 
+        //-> preEnd < start  ; 새롭게 만들기
+        
+        int [] pre = intervals[0];
+        merged.add(pre);
+        
+        for (int i = 1; i < intervals.length; i++) {
+            int[] curr = intervals[i];
+            if (pre[1] >= curr[0]) {
+                pre[1] = Math.max(pre[1], curr[1]);
+            } else {
+                pre = curr;
+                merged.add(pre);
+            }
+        }
+        
+        return merged.toArray(new int[merged.size()][]);
+    }
+    // TIme: O(NlogN)
+}
