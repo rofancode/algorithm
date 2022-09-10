f696683de2ec8ffaff11692f5a2bc395654dba9d
rofancode <75173954+rofancode@users.noreply.github.com>
2022-09-10 22:08:46 +0900

    Time: 3 ms (20.86%), Space: 46.2 MB (33.85%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.java
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -32,4 +32,7 @@ public class Solution {
         
         return false;
     }
+    //time complexity = O(N) / space complexity = O(1)
+    //slow가 circle 시작점 들어왔을 때 이미 fast는 circle안에 있고, slow가 circle을 한바퀴돌기 전에 fast와 만납니다. 
+    //tail Node 수 = T, circle Node수 = C Max 1바퀴에 만난다고 해도 T+C = N 
 }
