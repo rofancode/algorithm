581203107c0251124c8a6535da47e5861a78009c
rofancode <75173954+rofancode@users.noreply.github.com>
2022-09-10 22:10:52 +0900

    Time: 0 ms (100.00%), Space: 45.9 MB (51.89%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.java
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -11,26 +11,25 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
+        if (head == null) return false;
+        
         ListNode slow = head;
-        ListNode fast = head;
+        ListNode fast = head.next;
         
         // 서로 다른 속도 출발하는 node를 만들고
         // cycle이라면 결국에 돌고돌아서 두 node가 만나는 지점이 생길것
         // cycle이 아니라면 빠른쪽이 먼저 null 에 도착하겠지.
-        if (fast == null || fast.next == null) return false;
-        
-        slow = slow.next;
-        fast = fast.next.next;
-        
-        while (fast != null && fast.next != null) {
-            //같은 것을 찾을 때만 true return 
-            if(slow == fast) 
-                return true;
+                
+        while (fast != slow) {
+            //fast가 null 이되면 flase return 
+            if (fast == null || fast.next == null) 
+                return false;
+            
             slow = slow.next;
             fast = fast.next.next;
-        }
+        } //같을 때 loof가 끝나니까 
         
-        return false;
+        return true;
     }
     //time complexity = O(N) / space complexity = O(1)
     //slow가 circle 시작점 들어왔을 때 이미 fast는 circle안에 있고, slow가 circle을 한바퀴돌기 전에 fast와 만납니다. 
