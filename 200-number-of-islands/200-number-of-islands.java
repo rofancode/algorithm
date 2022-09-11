7af3901be555e44b0c699ec4af75195d8bb276a1
rofancode <75173954+rofancode@users.noreply.github.com>
2022-09-12 00:22:17 +0900

    Time: 6 ms (39.14%), Space: 57.6 MB (44.65%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -2,9 +2,7 @@ class Solution {
     char[][] grid;
     int[][] dirs;
     public int numIslands(char[][] grid) {
-        // 1일 때 섬, 0은 물 
-        // cnt 
-        // loof 
+
         this.grid = grid;
         this.dirs = new int[][]{{0,-1},{0, 1},{-1, 0},{1,0}};
     
@@ -14,21 +12,12 @@ class Solution {
                 if (grid[i][j] == '1') {
                     findIsland(i, j);
                     cnt++;
-                    //print();
                 }
             }
         }
         return cnt;
-    }
-    // public void print(){
-    //     for (int i = 0; i < grid.length; i++) {
-    //         for (int j = 0; j < grid[0].length; j++) {
-    //             System.out.print(grid[i][j] + " ");
-    //         }
-    //         System.out.println();
-    //     }
-    //     System.out.println();
-    // }
+    } // T O(MN) //S O(8) -> O(1)
+
     public void findIsland(int i, int j) {
         // 1일 때 2로 변경하고, 
         grid[i][j] ='2';
@@ -37,7 +26,6 @@ class Solution {
         for (int [] dir : dirs ) {
             int x = i + dir[0];
             int y = j + dir[1];
-            //System.out.println(">>>>> "+x+" | "+y);
             
             if(x >= 0 && x < grid.length && y >= 0 && y < grid[0].length && grid[x][y] == '1') {
                 findIsland(x, y);
