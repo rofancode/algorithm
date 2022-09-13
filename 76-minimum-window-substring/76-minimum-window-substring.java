4897527914bfc1d9a69fa4dd6aeabdcea213227e
rofancode <75173954+rofancode@users.noreply.github.com>
2022-09-13 12:23:19 +0900

    Time: 39 ms (23.74%), Space: 47.4 MB (38.16%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.java
+++ 76-minimum-window-substring/76-minimum-window-substring.java
@@ -1,72 +1,71 @@
 class Solution {
     public String minWindow(String s, String t) {
-
-        if (s.length() == 0 || t.length() == 0) {
-            return "";
-        }
-
-        // Dictionary which keeps a count of all the unique characters in t.
+        if (s.length() == 0 || t.length() == 0) return "";
+        // t 값을 전부 HashMap에 넣고, cnt도 넣음 
+        // s하나씩 뽑아서 확인
+        // idea 는 
+        /*
+        left랑 right pointer가 0에서 시작함, 
+        //1. 일단 t에 해당하는 글자 1개 나올 때 까지 left++
+        2. t에 해당하는 글자 전부 포함할 때 까지 right++
+        -> window 정해짐
+        
+        left ++ 다음 t.의 글자 나올 때 까지 
+        해당 window에서 글자 전부 들어가 있는 지 check
+        - 아니면 2반복
+        아니면 left++
+        */
+        // 문제는 어떻게 window내에 t 다 있는 지 확인하느냐인데, 
+        // map을 2개 - 1개는 t용, 1개는 window용 
+        // checking - 1) t용map에 있는 글자인지 2) 중복되는 글자 수도 맞는 지 
+        
         Map<Character, Integer> dictT = new HashMap<Character, Integer>();
         for (int i = 0; i < t.length(); i++) {
-            int count = dictT.getOrDefault(t.charAt(i), 0);
-            dictT.put(t.charAt(i), count + 1);
+            char c = t.charAt(i);
+            dictT.put(c, dictT.getOrDefault(c, 0) + 1);
         }
+        
+        int required = dictT.size(); //필요한 글자수
 
-        // Number of unique characters in t, which need to be present in the desired window.
-        int required = dictT.size();
-
-        // Left and Right pointer
-        int l = 0, r = 0;
-
-        // formed is used to keep track of how many unique characters in t
-        // are present in the current window in its desired frequency.
-        // e.g. if t is "AABC" then the window must have two A's, one B and one C.
-        // Thus formed would be = 3 when all these conditions are met.
-        int formed = 0;
-
-        // Dictionary which keeps a count of all the unique characters in the current window.
-        Map<Character, Integer> windowCounts = new HashMap<Character, Integer>();
-
-        // ans list of the form (window length, left, right)
-        int[] ans = { -1, 0, 0 };
-
+        int l = 0, r = 0; //pointers
+        
+        int cnt = 0; //window내 글자 cnt 체크 
+        
+        Map <Character, Integer> winCnt = new HashMap();
+        //저장해둔 값 비교해서 가장 작은 값일 때 글자 추출해야함
+        int [] win = {-1, 0, 0}; //winSize, l, r, 
+        
         while (r < s.length()) {
-            // Add one character from the right to the window
             char c = s.charAt(r);
-            int count = windowCounts.getOrDefault(c, 0);
-            windowCounts.put(c, count + 1);
-
-            // If the frequency of the current character added equals to the
-            // desired count in t then increment the formed count by 1.
-            if (dictT.containsKey(c) && windowCounts.get(c).intValue() == dictT.get(c).intValue()) {
-                formed++;
+            winCnt.put(c, winCnt.getOrDefault(c, 0) + 1);
+            
+            //t가 갖고 있고, 글자수도 같으면
+            if (dictT.containsKey(c) && winCnt.get(c).intValue() == dictT.get(c).intValue()) {
+                cnt++;
             }
-
-            // Try and contract the window till the point where it ceases to be 'desirable'.
-            while (l <= r && formed == required) {
+                
+            // 글자 다 포함하면, 글자 1개 누락할 때까지 left++          
+            while (l <= r && cnt == required) {
                 c = s.charAt(l);
-                // Save the smallest window until now.
-                if (ans[0] == -1 || r - l + 1 < ans[0]) {
-                    ans[0] = r - l + 1;
-                    ans[1] = l;
-                    ans[2] = r;
+                //처음이거나 window 저장 지금보다 작은 경우,  
+                if (win[0] == -1 || r - l + 1 < win[0]) {
+                        win[0] = r - l + 1;
+                        win[1] = l;
+                        win[2] = r;
                 }
-
-                // The character at the position pointed by the
-                // `Left` pointer is no longer a part of the window.
-                windowCounts.put(c, windowCounts.get(c) - 1);
-                if (dictT.containsKey(c) && windowCounts.get(c).intValue() < dictT.get(c).intValue()) {
-                    formed--;
+                //left이동으로 해당 글자 제외
+                winCnt.put(c, winCnt.get(c) - 1);
+                if (dictT.containsKey(c) && winCnt.get(c).intValue() < dictT.get(c).intValue()) {
+                    cnt--;
                 }
-
-                // Move the left pointer ahead, this would help to look for a new window.
+ 
                 l++;
             }
-
-            // Keep expanding the window once we are done contracting.
+    
             r++;
+            
         }
-
-        return ans[0] == -1 ? "" : s.substring(ans[1], ans[2] + 1);
+        
+        return win[0] == -1 ? "" : s.substring(win[1], win[2] + 1);
     }
 }
