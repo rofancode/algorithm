6d0482e794fc52a00e339612fed29d4f83d0a4a3
rofancode <75173954+rofancode@users.noreply.github.com>
2022-09-13 21:15:45 +0900

    Time: 46 ms (6.18%), Space: 50.2 MB (67.38%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -3,26 +3,27 @@ class Solution {
         //HashMap 에 저장 
         //HashMap value로 내림차순 정렬
         //K만큼 선택
+        if (k == nums.length) return nums;
         
-        Map<Integer, Integer> freq = new HashMap<>();
+        Map<Integer, Integer> count = new HashMap<>();
         
-        for (int i = 0; i < nums.length; i++) {
-            freq.put(nums[i], freq.getOrDefault(nums[i], 1) + 1);
+        for (int i = 0; i < nums.length; i++) { //O(N)
+            count.put(nums[i], count.getOrDefault(nums[i], 0) + 1);
         }
         
-        List <Map.Entry<Integer, Integer>> list = new LinkedList(freq.entrySet());
+        Queue<Integer> heap = new PriorityQueue<>((a, b) -> count.get(a) - count.get(b));
+        //완 전 쩐다!ㅋㅋ
+        for (int n : count.keySet()) { // heap k만큼 크기 유지 - 삽입 삭제 > O(logk) * N => O(NlogK)
+            heap.add(n);
+            if (heap.size() > k) heap.poll();
+        }
         
-        Collections.sort(list, (a, b) -> b.getValue() - a.getValue());
-        
-        int i = 0;
         int[] res = new int[k];
-        for (Map.Entry<Integer, Integer> map : list) {
-            if (i >= k) 
-                break;
-            res[i] = map.getKey();
-            i++;
+        for (int i = k - 1; i >= 0; i--) {
+            res[i] = heap.poll();
         }
-        
         return res;
     }
+    //time complexity = O(NlogK) -> O(NlogN)
+    //space complexity = O(N + k)
 }
